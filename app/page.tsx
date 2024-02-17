8ef9047077fd7dfd361feb88af67a4221940fdb0
Adegoke Ayomiposi <138511031+ayomiposi124@users.noreply.github.com>
2024-02-16 16:55:01 -0800

    👌Update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   <p>I am a <ins>programmer</ins></p>
   <p>water is H <sup>2</sup> 0</p>
   <p>oxygen is 0 <sub>2</sub></p>
+  <p>Hello my name is <b>Ayomiposi</b> </p>
 
   </>
     
