bc19212add0178e36be198a20666bbd17a9c6ad6
Adegoke Ayomiposi <138511031+ayomiposi124@users.noreply.github.com>
2024-02-21 05:50:17 -0800

    🤦‍♀️ Admend some correction

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   <p><bdo dir="rtl">This text will be written from right to left</bdo>  using bdo tag</p>
   <p>WWW is a: <q>World Wide Web.</q></p>
   <p>What is Biology?</p>
-  <blockquote >"Biology" is the scientific study of living organisms and their interactions with each other and their environments. It is a broad field that encompasses various levels of biological organization, from molecules and cells to organisms, populations, and ecosystems.</blockquote>
+  <blockquote >Biology is the scientific study of living organisms and their interactions with each other and their environments. It is a broad field that encompasses various levels of biological organization, from molecules and cells to organisms, populations, and ecosystems.</blockquote>
   <p><cite>The Scream</cite> by Adegoke Ayomiposi found in year 1999.</p>
 
   </>
